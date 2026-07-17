1ff462e5cbdfdf8021d3201cf0ce27da5c9c80d4
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-17 11:52:11 +0200

    added authorization in filter chain

--- src/main/java/robertovisconti/be_u5_w3_d5/security/SecurityConfig.java
+++ src/main/java/robertovisconti/be_u5_w3_d5/security/SecurityConfig.java
@@ -9,13 +9,21 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // Ci permette di configurare le impostazioni di spring security
 public class SecurityConfig {
 
+    private final TokenFilter tokenFilter;
+
+    public SecurityConfig(TokenFilter tokenFilter) {
+        this.tokenFilter = tokenFilter;
+    }
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
 
         // disabilito per non far gestire l'autenticazione tramite i classici form generati dal server, come la pagina di login che mi crea di default spring
@@ -29,7 +37,9 @@ public class SecurityConfig {
         httpSecurity.csrf(csrf -> csrf.disable());
 
         // Vado ad eliminare il controllo di autenticazione su tutti gli end-point che fà Spring Security di default (401)
-        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
+        httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll().anyRequest().authenticated());
+
+        httpSecurity.addFilterBefore(tokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
